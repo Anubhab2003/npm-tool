c546b25e40e7eefa86c49d95c0ae376cd09ba62e
Anubhab2003 <anubhabchowdhury01@gmail.com>
2024-11-12 23:13:28 +0530

    bugg fixed

--- index.js
+++ index.js
@@ -1,4 +1,15 @@
-function cramtx(){
-    console.log('Hello Users');
+import * as readline from 'readline'; // Import the entire readline module
+
+async function cramtx() {
+  const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+  });
+
+  rl.question('Please enter your name: ', (name) => {
+    console.log(`Hello ${name}!`);
+    rl.close();
+  });
 }
-export default cramtx
+
+cramtx(); // Execute the function
